0d95999154fc59a2892b47645e4a5973206ad135
miaochenlu <3170102668@zju.edu.cn>
2020-01-02 11:21:20 +0800

    add COMNET application layer

--- tools/assert-url.js
+++ tools/assert-url.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 
 var imgRg = /!\[([^\]]*)\]\((\/[^/]\S*)\)/gm;
-var basePath = 'https://raw.githubusercontent.com/kitian616/jekyll-TeXt-theme/master';
+var basePath = 'https://raw.githubusercontent.com/miaochenlu/miaochenlu.github.io/master';
 var srcPaths = ['./test/_posts', './docs/en', './docs/zh'];
 
 function runner() {
